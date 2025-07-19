3d5b09673bef879e437218e18090b4b26359d5cb
FathimahAzzahraWinarno <fathimahazzahra425@gmail.com>
2025-07-19 19:59:11 +0700

    update footer

---
+++ src/components/contactSection.tsx
@@ -0,0 +1,10 @@
+export default function contactSection () {
+    return(
+        <section
+            id="contact"
+            className="scroll-mt-24 min-h-screen flex flex-col items-center justify-center border-b px-4"
+        >
+            <h2 className="text-4xl font-bold mb-8 text-center">Contact</h2>
+        </section>
+    );
+}
