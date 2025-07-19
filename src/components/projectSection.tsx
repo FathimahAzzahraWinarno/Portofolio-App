179c77149b986b98aa07c14d6b5253ca3b39dee1
FathimahAzzahraWinarno <fathimahazzahra425@gmail.com>
2025-07-19 19:44:39 +0700

    add projectSection components

---
+++ src/components/projectSection.tsx
@@ -0,0 +1,10 @@
+export default function projectSection () {
+    return(
+        <section
+            id="project"
+            className="scroll-mt-24 min-h-screen flex flex-col items-center justify-center border-b px-4"
+        >
+            <h2 className="text-4xl font-bold mb-8 text-center">My Project</h2>
+        </section>
+    );
+}
